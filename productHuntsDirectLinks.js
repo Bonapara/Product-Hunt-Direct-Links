6422f91197b7eb11e526fedeb108ba1704499a11
Bonapara <tdesfrancs@gmail.com>
2020-10-01 14:09:58 +0200

    Create productHuntsDirectLinks.js

---
+++ productHuntsDirectLinks.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Product Hunt direct Links
+// @namespace    http://www.weekendr.eu
+// @version      0.1
+// @description  try to take over the world!
+// @author       Weekendr
+// @match        https://www.producthunt.com/
+// @grant        none
+// ==/UserScript==
+
+
+var links = document.querySelectorAll(".item_54fdd .minorActions_57e55")
+for (i = 0; i < links.length; ++i) {
+    links[i].style.display = "block";
+}
